bfa0cab441b79a1703d3101c6dd28f3b40b3c00c
김상욱 <kimsangouk1020@gmail.com>
2025-03-16 16:19:20 +0900

    chore: [S12P21A508-59] statics 프로젝트 생성
    - spring boot 기본 프로젝트 생성

---
+++ backend/statics-server/src/test/java/com/ssafy/staticsserver/StaticsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.staticsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
