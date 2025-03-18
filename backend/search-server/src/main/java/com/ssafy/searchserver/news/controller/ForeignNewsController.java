029e58775786acc9dea688527bc595fe6e6ad4c8
g1767g <g1767g@naver.com>
2025-03-18 21:37:15 +0900

    feat: [S12P21A508-75] 케í카테고리 필터링 구현

--- backend/search-server/src/main/java/com/ssafy/searchserver/news/controller/ForeignNewsController.java
+++ backend/search-server/src/main/java/com/ssafy/searchserver/news/controller/ForeignNewsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.searchserver.news.dto.ForeignNewsListResponse;
+import com.ssafy.searchserver.news.dto.ForeignNewsResponse;
 import com.ssafy.searchserver.news.entity.ForeignNewsElastic;
 import com.ssafy.searchserver.news.entity.ForeignNewsMongo;
 import com.ssafy.searchserver.news.service.ForeignNewsServiceImpl;
@@ -23,7 +24,7 @@ public class ForeignNewsController {
 
     private final ForeignNewsServiceImpl service;
 
-
+    //mongo
     @PostMapping("/mongo")
     public ForeignNewsMongo save(@RequestBody ForeignNewsMongo news) {
         return service.save(news);
@@ -37,24 +38,30 @@ public class ForeignNewsController {
                 .body(response);
     }
 
+    //elasticsearch
+    @PostMapping("es")
+    public ForeignNewsResponse saveNews(@RequestBody ForeignNewsElastic news) {
+        return service.save(news);
+    }
 
-    // @PostMapping("es")
-    // public ForeignNewsElastic saveNews(@RequestBody ForeignNewsElastic news) {
-    //     return service.save(news);
-    // }
-    //
-    //
-    // @GetMapping("/es/keyword")
-    // public ResponseEntity<ForeignNewsListResponse> getNewsByKeyword(@RequestParam String keyword) {
-    //     ForeignNewsListResponse response = service.searchByKeyword(keyword);
-    //     return ResponseEntity.status(HttpStatus.OK).body(response);
-    // }
-    //
-    // @GetMapping("/es/category")
-    // public ResponseEntity<ForeignNewsListResponse> getNewsByCategory(@RequestParam String category) {
-    //     ForeignNewsListResponse response = service.searchByCategory(category);
-    //     return ResponseEntity.status(HttpStatus.OK).body(response);
-    // }
+
+    @GetMapping("/es/keyword")
+    public ResponseEntity<ForeignNewsListResponse> getNewsByKeyword(@RequestParam String keyword) {
+        ForeignNewsListResponse response = service.searchByKeyword(keyword);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/es/category")
+    public ResponseEntity<ForeignNewsListResponse> getNewsByCategory(@RequestParam String category) {
+        ForeignNewsListResponse response = service.searchByCategory(category);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/es/period")
+    public ResponseEntity<ForeignNewsListResponse> getNewsByPeriod(@RequestParam int period) {
+        ForeignNewsListResponse response = service.searchByPeriod(period);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
 
 }
--- backend/search-server/src/main/java/com/ssafy/searchserver/news/repository/ForeignNewsElasticsearchRepository.java
+++ backend/search-server/src/main/java/com/ssafy/searchserver/news/repository/ForeignNewsElasticsearchRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.searchserver.news.repository;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
@@ -7,6 +8,9 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import com.ssafy.searchserver.news.entity.ForeignNewsElastic;
 
 public interface ForeignNewsElasticsearchRepository extends ElasticsearchRepository<ForeignNewsElastic, String> {
-	List<ForeignNewsElastic> findByKeywordsContaining(String keyword);
-	List<ForeignNewsElastic> findByCategoriesContaining(String category);
+	List<ForeignNewsElastic> findByKeywords(String keyword);
+	List<ForeignNewsElastic> findByCategories(String category);
+	List<ForeignNewsElastic> findByPublishedAtBetween(Instant from, Instant to);
+
+
 }
