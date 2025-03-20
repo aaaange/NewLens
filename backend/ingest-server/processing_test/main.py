d3207f516f1b4f9f9a384db43f5a57a26e117406
김보민 <mokbee1@naver.com>
2025-03-20 21:29:45 +0900

    feat: [S12P21A508-105] 국가 추출 추가

--- backend/ingest-server/processing_test/main.py
+++ backend/ingest-server/processing_test/main.py
@@ -4,6 +4,7 @@ from keyword_extraction import extract_keywords
 from sentiment import extract_sentiment
 from translation import translate
 from utils import format_published_at
+from country_extraction import determine_source_country
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
@@ -48,6 +49,9 @@ def main():
     # Komoran + TF-IDF를 이용한 핵심 키워드 추출 (최대 5개)
     keywords = extract_keywords(text, max_keywords=10)
 
+    # 국가 추출
+    country = determine_source_country(news_item["source"])
+
     # mongoDB에 데이터 입력
     collection.insert_one(
         {
@@ -57,6 +61,7 @@ def main():
             "published_at": formatted_date,
             "image_url": news_item["image_url"],
             "categories": categories,
+            "country": country,
             "keywords": keywords,
             "sentiment": sentiment_dic,
             "raw_data_ref": news_item["uuid"],
@@ -64,15 +69,16 @@ def main():
     )
 
     # 결과 출력
-    print("UUID:", news_item["uuid"])
-    print("제목:", translate({news_item["language"]: news_item["title"]}))
-    print("요약:", translate({news_item["language"]: news_item["description"]}))
-    print("발행일시:", formatted_date)
-    print("ImageUrl:", news_item["image_url"])
-    print("분류된 카테고리:", categories)
-    print("핵심 키워드:", keywords)
-    print("감성 점수:", sentiment_scores)
-    print("URL:", news_item["url"])
+    # print("UUID:", news_item["uuid"])
+    # print("제목:", translate({news_item["language"]: news_item["title"]}))
+    # print("요약:", translate({news_item["language"]: news_item["description"]}))
+    # print("발행일시:", formatted_date)
+    # print("ImageUrl:", news_item["image_url"])
+    # print("분류된 카테고리:", categories)
+    # print("국가: ", country)
+    # print("핵심 키워드:", keywords)
+    # print("감성 점수:", sentiment_scores)
+    # print("URL:", news_item["url"])
 
 
 if __name__ == "__main__":
