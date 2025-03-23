8ff6737b0410f18db5cc4b0b66d547ac15c9e644
김보민 <mokbee1@naver.com>
2025-03-24 00:58:57 +0900

    feat: [S12P21A508-115] 언급량 response

---
+++ backend/search-server/src/main/java/com/ssafy/searchserver/search/dto/MentionResponse.java
@@ -0,0 +1,16 @@
+package com.ssafy.searchserver.search.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "Mention 응답 데이터")
+public class MentionResponse {
+	@Schema(description = "국가 코드", example = "ko")
+	private String name;
+
+	@Schema(description = "언급 수", example = "125")
+	private int count;
+}
