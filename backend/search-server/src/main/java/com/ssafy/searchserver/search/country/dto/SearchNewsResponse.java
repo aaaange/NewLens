2b0516a22c4a0911923ac3895aab8c35f5da9fc8
김보민 <mokbee1@naver.com>
2025-03-24 03:04:18 +0900

    feat: [S12P21A508-115] 뉴스 목록 response
    Former-commit-id: 07c48d464405d222629f1d0af9177b0b9a68c4d0

---
+++ backend/search-server/src/main/java/com/ssafy/searchserver/search/country/dto/SearchNewsResponse.java
@@ -0,0 +1,22 @@
+package com.ssafy.searchserver.search.country.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "뉴스 검색 응답")
+public class SearchNewsResponse {
+	@Schema(description = "응답 코드", example = "SUCCESS")
+	private String code;
+
+	@Schema(description = "성공 여부", example = "true")
+	private Boolean success;
+
+	@Schema(description = "응답 메시지", example = "요청 성공")
+	private String message;
+
+	@Schema(description = "뉴스 데이터")
+	private NewsData data;
+}
