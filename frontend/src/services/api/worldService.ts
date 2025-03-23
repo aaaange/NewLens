01e2a547fa492c097ea8bd7ff536825ec1523455
seungju <seungju425@gmail.com>
2025-03-23 11:16:18 +0900

    feat: [S12P21A508-108] 더미데이터를 이용하여 감정 수치를 지도에 표시
    - resonse를 이용하여 primarysentiment를 찾고, 해당 감정에 따른 지도 색상 변경 로직 작성
    - interface, type, api 함수 services 폴더 파일로 분리
    
    
    Former-commit-id: 1075c8fcff7bb275b2ae6db521cb9b136e36281e

--- frontend/src/services/api/worldService.ts
+++ frontend/src/services/api/worldService.ts
@@ -1,1 +1,40 @@
 // world에 관련한 api를 작성하는 곳.
+import { api, multipartApi } from './Api';
+
+//==============================================
+// 인터페이스 정의
+//==============================================
+export interface worldMentionType {
+  [key: string]: number;
+}
+
+export interface countryNameType {
+  [key: string]: string;
+}
+
+export type SentimentName = 'positive' | 'neutral' | 'negative';
+
+// 특정 국가의 감정 타입
+export interface sentimentType {
+  positive: number;
+  neutral: number;
+  negative: number;
+  primarySentiment: SentimentName;
+}
+
+export interface sentimentResType extends sentimentType {
+  name: string;
+}
+
+// 모든 국가의 감정 타입
+export interface worldSentimentType {
+  [countryCode: string]: sentimentType;
+}
+
+//==============================================
+// API 정의
+//==============================================
+export const getWorldMapData = async () => {
+  const response = await api.get(`search/worldwide`);
+  return response.data;
+};
