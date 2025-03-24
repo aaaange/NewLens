40cab56882b875224328ee060431870c47d20a34
aaange <ayung00@naver.com>
2025-03-24 15:24:12 +0900

    feat: [S12P21A508-125] 검색 컴포넌트 배치

--- frontend/src/pages/WorldDetail.tsx
+++ frontend/src/pages/WorldDetail.tsx
@@ -4,6 +4,10 @@ import GptSummary from '../components/worldDetail/GptSummary';
 import FirstCountryBoard from '../components/worldDetail/FirstCountryDashboard';
 import SecondCountryBoard from '../components/worldDetail/SecondCountryDashboard';
 import { keyword as mockKeyword } from '../components/worldDetail/MockData';
+import SearchInput from '../components/common/SearchInput';
+import MindMap from '../components/common/MindMap';
+import KeywordRanking from '../components/common/KeywordRanking';
+import Category from '../components/common/Category';
 
 const description: string =
   "봄꽃이 개화하는 시기에 국내 여행객들이 가장 많이 찾는 여행지가 '제주도'라는 조사 결과가 나왔다. 12일 글로벌 여행 플랫폼 트립닷컴은 오는 25일~다음 달 30일 국내 여행객의 여행 추이를 공개했다. 제주시와 서귀포시가 1, 2위에 올랐다 지난해는 반대로 서귀포시가 1위, 제주시가 2위였다. 다음으로는 서울과 부산이 뒤를 이었다.";
@@ -18,8 +22,14 @@ const WorldDetail = () => {
   const [secondCountryName, setSecondCountryName] = useState('미국');
 
   return (
-    <div className="mt-5">
+    <div className="mt-5 flex gap-10 justify-center">
+      <div className="flex flex-col gap-5">
+        <SearchInput />
+        <MindMap />
+        <KeywordRanking />
+      </div>
       <div className="flex flex-col items-center gap-3">
+        <Category />
         <div className="flex gap-10">
           <CountryDropdown
             width="410px"
