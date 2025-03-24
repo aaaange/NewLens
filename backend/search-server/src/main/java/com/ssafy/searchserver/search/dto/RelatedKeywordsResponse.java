f10764d2abdb7692b0eb71c2182827bb9aa26643
g1767g <g1767g@naver.com>
2025-03-24 20:08:41 +0900

    feat: [S12P21A508-133] 연관어 추출 기능 구현

---
+++ backend/search-server/src/main/java/com/ssafy/searchserver/search/dto/RelatedKeywordsResponse.java
@@ -0,0 +1,17 @@
+package com.ssafy.searchserver.search.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RelatedKeywordsResponse {
+	private String keyword;
+	private List<String> relatedKeywords;
+}
