fa984247f63e9c7b0a2dd7094ef1b0190eef2f92
aaange <ayung00@naver.com>
2025-03-24 13:51:20 +0900

    chore: [S12P21A508-125] 컴포넌트 헤더 디테일 수정

--- frontend/src/components/worldDetail/StackedColumns.tsx
+++ frontend/src/components/worldDetail/StackedColumns.tsx
@@ -88,11 +88,11 @@ const StackedColumns = ({
 
   return (
     <>
-      <p className="flex">
+      <p className="flex items-center">
         <Flag code={country_code} width="24" height="12" /> &nbsp;
         {country_name}에서 본&nbsp;
-        <span className="text-system-warning">{keyword}</span>에 대한&nbsp;
-        <span className="text-system-warning">감정 분석</span>
+        <span className="text-amount-300 text-lg ">{keyword}</span>에 대한&nbsp;
+        <span className="text-amount-300 text-lg ">감정 분석</span>
       </p>
       <Chart
         options={options}
