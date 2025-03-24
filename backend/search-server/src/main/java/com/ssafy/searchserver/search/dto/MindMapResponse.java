2b9a40edd8fad2d5746c8208730afae8c0980310
g1767g <g1767g@naver.com>
2025-03-24 17:00:44 +0900

    refactor: [S12P21A508-123] SearchController 스웨거 통합 및 서버 포트 8080 변경

--- backend/search-server/src/main/java/com/ssafy/searchserver/search/dto/MindMapResponse.java
+++ backend/search-server/src/main/java/com/ssafy/searchserver/search/dto/MindMapResponse.java
@@ -1,6 +1,7 @@
 package com.ssafy.searchserver.search.dto;
 
 import java.util.List;
+import java.util.Map;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -19,6 +20,6 @@ public class MindMapResponse {
 	@Schema(description = "응답 메시지", example = "요청 성공")
 	private String message;
 
-	@Schema(description = "연관 키워드 리스트", example = "[\"관세\", \"도널드\", \"머스크\"]")
-	private List<String> data;
+	@Schema(description = "연관 키워드 리스트", example = "keywords: [\"관세\", \"도널드\", \"머스크\"]")
+	private Map<String, List<String>> data;
 }
