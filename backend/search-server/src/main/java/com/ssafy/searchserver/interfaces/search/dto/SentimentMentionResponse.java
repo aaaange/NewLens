0fa1dfb9c73265b3bdd26896567273332677be4a
g1767g <g1767g@naver.com>
2025-03-25 00:06:57 +0900

    refactor: [S12P21A508-134] 검색 서버 프로ì패키지  DDëD DD로 변경

---
+++ backend/search-server/src/main/java/com/ssafy/searchserver/interfaces/search/dto/SentimentMentionResponse.java
@@ -0,0 +1,22 @@
+package com.ssafy.searchserver.interfaces.search.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "Sentiment 및 Mention Response")
+public class SentimentMentionResponse {
+	@Schema(description = "응답 코드", example = "SUCCESS")
+	private String code;
+
+	@Schema(description = "성공 여부", example = "true")
+	private Boolean success;
+
+	@Schema(description = "응답 메시지", example = "요청 성공")
+	private String message;
+
+	@Schema(description = "데이터")
+	private SentimentMentionData data;
+}
