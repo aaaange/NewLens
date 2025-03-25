2aef3b1ae9f2cb02eea69961abfc8769af284081
seungju <seungju425@gmail.com>
2025-03-25 13:33:57 +0900

    feat: [S12P21A508-141] 세계지도 api 연결 수정
    - 메인 페이지에서 받은 데이터 지도와 연결
    - interface 이름 수정 및 타입 수정
    - lottiefiles 패키지 설치
    - mapswitchtab 컴포넌트에 lottiefiles 작성

--- frontend/src/services/api/worldService.ts
+++ frontend/src/services/api/worldService.ts
@@ -5,13 +5,29 @@ import { api, multipartApi } from './Api';
 // 인터페이스 정의
 //==============================================
 
-export interface worldMentionType {
+export type mentionResType = { name: string; count: number }[];
+
+export interface mentionObjType {
   [key: string]: number;
 }
 
 export interface countryNameType {
   [key: string]: string;
 }
+
+export type sentimentResType = {
+  name: string;
+  positive: number;
+  neutral: number;
+  negative: number;
+}[];
+
+// export interface worldSentimentDataType {
+//   name: string;
+//   positive: number;
+//   neutral: number;
+//   negative: number;
+// }
 
 export type SentimentName = 'positive' | 'neutral' | 'negative';
 
@@ -23,12 +39,12 @@ export interface sentimentType {
   primarySentiment: SentimentName;
 }
 
-export interface sentimentResType extends sentimentType {
-  name: string;
-}
+// export interface sentimentResType extends sentimentType {
+//   name: string;
+// }
 
 // 모든 국가의 감정 타입
-export interface worldSentimentType {
+export interface sentimentObjType {
   [countryCode: string]: sentimentType;
 }
 
