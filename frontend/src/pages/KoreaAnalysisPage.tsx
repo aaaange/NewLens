6f27c0a4fc3e0321a5cfe0cff43b521c27d97073
aaange <ayung00@naver.com>
2025-03-25 11:31:31 +0900

    feat: [S12P21A508-145] 한국 특화 페이지 UI 구현

--- frontend/src/pages/KoreaAnalysisPage.tsx
+++ frontend/src/pages/KoreaAnalysisPage.tsx
@@ -1,11 +1,33 @@
-import KoreaAnalysis from "../components/korea/KoreaAnalysis";
+import KoreaAnalysis from '../components/korea/KoreaAnalysis';
+import SearchInput from '../components/common/SearchInput';
+import MindMap from '../components/common/MindMap';
+import KeywordRanking from '../components/common/KeywordRanking';
+import { useParams } from 'react-router-dom';
 
 const KoreaAnalysisPage = () => {
-    return (
-        <>
-        <KoreaAnalysis />
-        </>
-    )
+  // const { category, period, keyword } = useParams();
+  const { category, period } = useParams();
+
+  const country: string = 'kr';
+  const country_name: string = '대한민국';
+  const keyword: string = '임시 데이터';
+
+  return (
+    <div className="mt-5 flex gap-10 justify-center">
+      <div className="flex flex-col gap-5">
+        <SearchInput />
+        <MindMap />
+        <KeywordRanking />
+      </div>
+      <KoreaAnalysis
+        country={country}
+        country_name={country_name}
+        keyword={keyword ?? ''} // undefined 방지
+        category={category ?? ''}
+        period={category ?? ''}
+      />
+    </div>
+  );
 };
 
 export default KoreaAnalysisPage;
