0bd0a86f618761ce006eee3877b4acc4b87b235e
김보민 <mokbee1@naver.com>
2025-03-26 10:40:09 +0900

    feat: [S12P21A508-154] 언급량 response 수정

--- backend/statics-server/src/main/java/com/ssafy/staticsserver/search/dto/MentionResponse.java
+++ backend/statics-server/src/main/java/com/ssafy/staticsserver/search/dto/MentionResponse.java
@@ -6,14 +6,12 @@ import lombok.Getter;
 @Getter
 @Builder
 public class MentionResponse {
-	private String keyword;
 	private String country;
 	private int count;
 
 	@Override
 	public String toString() {
 		return "MentionResponse{" +
-			"keyword='" + keyword + '\'' +
 			", country='" + country + '\'' +
 			", count=" + count +
 			'}';
