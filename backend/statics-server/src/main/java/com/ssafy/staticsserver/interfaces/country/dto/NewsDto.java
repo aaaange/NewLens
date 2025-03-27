708742187d716f675969d5e43dfa3244a6c30c17
g1767g <g1767g@naver.com>
2025-03-28 00:28:05 +0900

    refactor: [S12P21A508-172] Country 뉴스 모달창

--- backend/statics-server/src/main/java/com/ssafy/staticsserver/interfaces/country/dto/NewsDto.java
+++ backend/statics-server/src/main/java/com/ssafy/staticsserver/interfaces/country/dto/NewsDto.java
@@ -3,11 +3,14 @@ package com.ssafy.staticsserver.interfaces.country.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class NewsDto {
 	private String title;
 	private String url;
