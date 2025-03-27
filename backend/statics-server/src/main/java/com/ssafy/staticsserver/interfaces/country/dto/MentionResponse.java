5bc10efe8ae0e6b72eb2b5bf86960aea9f3a9b40
g1767g <g1767g@naver.com>
2025-03-28 00:33:22 +0900

    refactor: [S12P21A508-172] Country 국가별 대시보드
    Former-commit-id: 776388b034110e5e2b9d49d18f50918f73daf7a7

--- backend/statics-server/src/main/java/com/ssafy/staticsserver/interfaces/country/dto/MentionResponse.java
+++ backend/statics-server/src/main/java/com/ssafy/staticsserver/interfaces/country/dto/MentionResponse.java
@@ -2,11 +2,14 @@ package com.ssafy.staticsserver.interfaces.country.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class MentionResponse {
 	private LocalDateTime publishedAt;
 	private int count;
