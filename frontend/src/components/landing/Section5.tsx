da77fdf0a937b090556e6a2a9c0c4f739e15a4b1
seungju <seungju425@gmail.com>
2025-03-27 18:00:40 +0900

    feat: [S12P21A508-152] 랜딩페이지 작업
    - 스크롤 효과 적용
    - aos install
    
    
    Former-commit-id: 5bb087f8b51785f4afab6d3ca14c863341d17591

---
+++ frontend/src/components/landing/Section5.tsx
@@ -0,0 +1,25 @@
+import { forwardRef } from 'react';
+const Section5 = forwardRef<HTMLDivElement>((_, ref) => {
+  return (
+    <div ref={ref} className="h-screen border-b border-gray-400">
+      <div className="flex flex-col justify-center items-center gap-[40px] mt-[50px]">
+        <p className="display-small">
+          당신의 관심 뉴스, <span className="text-amount-300">맞춤형 추천</span>
+          부터 <span className="text-amount-300">스크랩</span>까지
+        </p>
+        <img
+          src="/assets/images/news1.png"
+          alt="뉴스 스크랩 이미지"
+          className="w-[600px] h-[200px]"
+        />
+        <img
+          src="/assets/images/news2.png"
+          alt="뉴스 스크랩 이미지"
+          className="w-[600px] h-[200px]"
+        />
+      </div>
+    </div>
+  );
+});
+
+export default Section5;
