896b1d96dcb584b0934ad845848849aae0e60013
김보민 <mokbee1@naver.com>
2025-03-27 16:12:34 +0900

    feat: [S12P21A508-164] static-server에 period 전달
    Former-commit-id: eb8bc938bf4c67e6410787a06b24187c8552974c

--- backend/search-server/src/main/java/com/ssafy/searchserver/application/country/CountryService.java
+++ backend/search-server/src/main/java/com/ssafy/searchserver/application/country/CountryService.java
@@ -116,8 +116,13 @@ public class CountryService {
 				.map(hit -> hit.source().getId())
 				.collect(Collectors.toList());
 
-			// kafka로 전송
-			String json = objectMapper.writeValueAsString(idList);
+			// kafka로 전달할 payload에 newsIds, page, size를 함께 포함
+			Map<String, Object> payload = new HashMap<>();
+			payload.put("newsIds", idList);
+			payload.put("period", period);
+
+			// Map을 JSON 문자열로 변환 & kafka로 전송
+			String json = objectMapper.writeValueAsString(payload);
 			kafkaTemplate.send("dashboard", json);
 
 			// 4. Aggregation 처리
@@ -139,8 +144,8 @@ public class CountryService {
 			for (KeywordResponse keywordResponse : wordCloud) {
 				System.out.println(keywordResponse.toString());
 			}
-			return DashboardData.builder().build();
 
+			return DashboardData.builder().keywords(wordCloud).build();
 		} catch (Exception e) {
 			throw new RuntimeException("Dashboard 데이터 검색 실패", e);
 		}
