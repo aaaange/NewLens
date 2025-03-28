bbfaad010140a31b0267f4255326453f00e06402
seungju <seungju425@gmail.com>
2025-03-28 09:27:02 +0900

    design: [S12P21A508-152] section3 flip up 애니메이션 적용
    Former-commit-id: 7d3f1db4b5cb107367f2700e4b7e8ce70b57e12d

--- frontend/src/components/landing/Section3.tsx
+++ frontend/src/components/landing/Section3.tsx
@@ -7,10 +7,7 @@ const Section3 = forwardRef<HTMLDivElement>((_, ref) => {
   }, []);
   return (
     <div ref={ref} className="h-screen border-b border-gray-400">
-      <div className="flex flex-col items-center gap-[100px]">
-        <div className="display-small" data-aos="flip-up" data-aos-delay="2100">
-          글로벌 <span className="text-amount-300">뉴스 흐름</span>을 한눈에!
-        </div>
+      <div className="flex flex-col items-center gap-[60px]">
         <div className="flex justify-center gap-[100px]">
           <div
             className="flex flex-col items-center gap-[50px]"
@@ -36,6 +33,9 @@ const Section3 = forwardRef<HTMLDivElement>((_, ref) => {
             <img src="/assets/images/keywordranking.png" alt="로고" />
             <p className="headline-xlarge">연관어 분석까지!</p>
           </div>
+        </div>
+        <div className="display-small" data-aos="flip-up" data-aos-delay="2900">
+          글로벌 <span className="text-amount-300">뉴스 흐름</span>을 한눈에!
         </div>
       </div>
     </div>
