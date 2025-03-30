d1c3118a2660664b4107d5fec002e7d99ea0466f
g1767g <g1767g@naver.com>
2025-03-30 15:18:38 +0900

    feat: [S12P21A508-184] 연관어 처리 - 날짜 포맷 변경 - text 타입 집계 불가 .keyword 붙여서 키워드 타입으로

--- backend/statics-server/src/main/java/com/ssafy/staticsserver/application/search/SearchService.java
+++ backend/statics-server/src/main/java/com/ssafy/staticsserver/application/search/SearchService.java
@@ -38,20 +38,6 @@ public class SearchService {
 	private static final List<String> G20_COUNTRIES = Collections.unmodifiableList(Arrays.asList(
 		"AR", "AU", "BR", "CA", "CN", "FR", "DE", "IN", "ID", "IT", "JP", "MX", "RU", "SA", "ZA", "KR", "TR", "GB", "US", "EU"));
 
-	@KafkaListener(topics = "related-keywords")
-	public void listenRelatedKeywords(String message) {
-		try {
-			List<String> newsIds = objectMapper.readValue(message, new TypeReference<List<String>>() {
-			});
-			List<ForeignNewsMongo> newsList = mongoDBRepository.findByIdIn(newsIds);
-			System.out.println("연관어 처리를 위한 뉴스 리스트");
-			for (ForeignNewsMongo foreignNewsMongo : newsList) {
-				System.out.println(foreignNewsMongo);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	@KafkaListener(topics = "keyword-ranking")
 	public void listenKeywordRanking(String message) {
