aed0dec48900deea4af3a7c9041e7bcc93e23745
young jo <a10019610@gmail.com>
2025-04-01 00:36:26 +0900

    feat: [S12P21A508-101] 뉴스 리스트 모달 연결 및 API 수정

--- frontend/src/components/worldDetail/NewsList.tsx
+++ frontend/src/components/worldDetail/NewsList.tsx
@@ -14,6 +14,7 @@ interface NewsListProps {
   keyword: string;
   country_name: string;
   country_code: string;
+  handleModalOpen: (country: string) => void; // 모달 열기 함수
 }
 const NewsList = ({
   news,
@@ -21,22 +22,24 @@ const NewsList = ({
   keyword,
   country_name,
   country_code,
+  handleModalOpen,
 }: NewsListProps) => {
+  const Flags = Flag as any; // Flag 컴포넌트의 타입을 any로 설정
   return (
     <div className="flex flex-col gap-2" style={{ width: `${width}px` }}>
       {/* 뉴스 리스트 헤더 */}
       <div className="flex flex-wrap justify-between ">
         <p className="flex items-center flex-wrap">
-          <Flag code={country_code} width="24" height="12" /> &nbsp;
+          <Flags code={country_code} width="24" height="12" /> &nbsp;
           <span className="text-lg "> {country_name}</span>에서 본&nbsp;
           <span className="text-amount-300 text-lg ">{keyword}</span>의
           관련&nbsp;
           <span className="text-amount-300 text-lg "> 뉴스</span>
         </p>
         <div className="ml-auto">
-          <a href="" className="p-2">
+          <div onClick={() => handleModalOpen(country_code)} className="p-2">
             + 더보기
-          </a>
+          </div>
         </div>
       </div>
 
