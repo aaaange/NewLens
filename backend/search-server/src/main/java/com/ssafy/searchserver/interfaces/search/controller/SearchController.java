68c0dfa6e62cb8aa137210cc6daba8b1ff349f41
g1767g <g1767g@naver.com>
2025-04-01 00:37:41 +0900

    refactor: [S12P21A508-191] SearchController 명세서 맞춤
    Former-commit-id: 255ece29e088c1dec33b33a243fb5a3f53f11e87

--- backend/search-server/src/main/java/com/ssafy/searchserver/interfaces/search/controller/SearchController.java
+++ backend/search-server/src/main/java/com/ssafy/searchserver/interfaces/search/controller/SearchController.java
@@ -1,5 +1,8 @@
 package com.ssafy.searchserver.interfaces.search.controller;
 
+import com.ssafy.searchserver.common.exeception.CustomException;
+import com.ssafy.searchserver.common.exeception.ErrorCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.ssafy.searchserver.common.dto.CommonResponse;
@@ -49,8 +52,8 @@ public class SearchController {
 		))
 	})
 
-	@GetMapping("/extract-related_words")
-	public CommonResponse<RelatedKeywordsResponse> getRelatedKeywords(
+	@GetMapping("/extract_related_words")
+	public ResponseEntity<CommonResponse<RelatedKeywordsResponse>> getRelatedKeywords(
 		@Parameter(description = "연관어를 추출할 기준 키워드", example = "트럼프")
 		@RequestParam String keyword,
 
@@ -58,35 +61,38 @@ public class SearchController {
 		@RequestParam String category,
 
 		@Parameter(description = "조회 기간 ex)1, 7, 30", example = "7")
-		@RequestParam(defaultValue = "7") int period,
+		@RequestParam int period,
 
 		@Parameter(description = "한국 특화 여부", example = "false")
-		@RequestParam(defaultValue = "false", name = "is_korea") boolean isKorea
+		@RequestParam(name = "is_korea") boolean isKorea
 	) {
 		RelatedKeywordsResponse data = service.getRelatedKeywords(keyword, category, period, isKorea);
-		return CommonResponse.success(data);
+		CommonResponse<RelatedKeywordsResponse> response = CommonResponse.success(data);
+		return ResponseEntity.ok(response);
 	}
 
 	@Operation(summary = "키워드 랭킹 조회", description = "카테고리, 기간, 국내/해외 뉴스에 따른 키워드 랭킹을 조회합니다.")
-	@GetMapping("/keyword-ranking")
-	public CommonResponse<KeywordRankingData> getKeywordRanking(
+	@GetMapping("/keyword_ranking")
+	public ResponseEntity<CommonResponse<KeywordRankingData>> getKeywordRanking(
 		@Parameter(description = "카테고리", example = "politics") @RequestParam String category,
 		@Parameter(description = "기간 (ex: 7)", example = "7") @RequestParam int period,
 		@Parameter(description = "한국 필터링 여부", example = "false") @RequestParam(name = "is_korea") boolean isKorea
 	) {
-		KeywordRankingData response = service.getKeywordRanking(category, period, isKorea);
-		return CommonResponse.success(response);
+		KeywordRankingData data = service.getKeywordRanking(category, period, isKorea);
+		CommonResponse<KeywordRankingData> response = CommonResponse.success(data);
+		return ResponseEntity.ok(response);
 	}
 
 	@Operation(summary = "세계 지도 정보 출력", description = "세계 지도에서 나라별 감성 분석 결과와 언급량 결과를 출력합니다.")
 	@GetMapping("/worldwide")
-	public CommonResponse<SentimentMentionData> getWorldwide(
+	public ResponseEntity<CommonResponse<SentimentMentionData>> getWorldwide(
 		@Parameter(description = "키워드", example = "트럼프") @RequestParam String keyword,
-		@Parameter(description = "키워드", example = "관세") @RequestParam(name = "keyword-mind") String keywordMind,
+		@Parameter(description = "마인드맵키워드", example = "관세") @RequestParam(name = "keyword_mind") String keywordMind,
 		@Parameter(description = "카테고리", example = "politics") @RequestParam String category,
 		@Parameter(description = "기간", example = "30") @RequestParam int period
 	) {
-		SentimentMentionData response = service.getWorldwide(keyword, keywordMind, category, period);
-		return CommonResponse.success(response);
+		SentimentMentionData data = service.getWorldwide(keyword, keywordMind, category, period);
+		CommonResponse<SentimentMentionData> response = CommonResponse.success(data);
+		return ResponseEntity.ok(response);
 	}
 }
