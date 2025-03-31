c51a94277e187cf88589a689f3feaeff16527f43
g1767g <g1767g@naver.com>
2025-04-01 03:10:24 +0900

    feat: [S12P21A508-194] Country 카테고리 all 적용
    Former-commit-id: 132a62e4edf979c35f2051c6d5b41fa653aef45e

--- backend/search-server/src/main/java/com/ssafy/searchserver/common/exeception/ErrorCode.java
+++ backend/search-server/src/main/java/com/ssafy/searchserver/common/exeception/ErrorCode.java
@@ -10,9 +10,9 @@ public enum ErrorCode {
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "INVALID_PARAMETER", "필수 파라미터가 누락ㅋ"),
     INVALID_METHOD(HttpStatus.METHOD_NOT_ALLOWED, "INVALID_METHOD", "GET POST 구분 합시다잉"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버 오류인데 내탓 아님ㅎ"),
-    INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "INVALID_CATEGORY", "카테고리 없음ㅋ"),
+    INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "INVALID_CATEGORY", "그런 카테고리 없음ㅋ"),
     INVALID_PERIOD(HttpStatus.BAD_REQUEST, "INVALID_PERIOD", "기간 제대로ㅋ"),
-    INVALID_COUNTRY(HttpStatus.BAD_REQUEST, "INVALID_COUNTRY", "G20 아님ㅋ"),
+    INVALID_COUNTRY(HttpStatus.BAD_REQUEST, "INVALID_COUNTRY", "그건 G20 아님ㅋ"),
     INVALID_PAGE(HttpStatus.BAD_REQUEST, "INVALID_PAGE", "유효하지 않은 페이지ㅋ");
 
     private final HttpStatus status;
