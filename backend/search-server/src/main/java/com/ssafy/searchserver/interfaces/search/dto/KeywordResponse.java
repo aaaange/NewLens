f67c245d9fe611cf3916ae6097cf127fdef34b55
김보민 <mokbee1@naver.com>
2025-04-02 11:31:26 +0900

    feat: [S12P21A508-176] 생성자 어노테이션 추가

--- backend/search-server/src/main/java/com/ssafy/searchserver/interfaces/search/dto/KeywordResponse.java
+++ backend/search-server/src/main/java/com/ssafy/searchserver/interfaces/search/dto/KeywordResponse.java
@@ -1,10 +1,14 @@
 package com.ssafy.searchserver.interfaces.search.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class KeywordResponse {
 	private String name;
 	private String state;
