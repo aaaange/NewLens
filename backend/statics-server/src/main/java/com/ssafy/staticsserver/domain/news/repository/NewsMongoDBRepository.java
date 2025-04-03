cbac88ea3706c8e8ee09f8bf2c6e4393105bfc0b
g1767g <g1767g@naver.com>
2025-04-04 00:41:24 +0900

    feat: [S12P21A508-204] 키워드 랭킹 빼고 연결 완료
    Former-commit-id: 29cdedb79d8270797e24da6503dfa9b22ec5d73c

--- backend/statics-server/src/main/java/com/ssafy/staticsserver/domain/news/repository/NewsMongoDBRepository.java
+++ backend/statics-server/src/main/java/com/ssafy/staticsserver/domain/news/repository/NewsMongoDBRepository.java
@@ -1,15 +1,12 @@
 package com.ssafy.staticsserver.domain.news.repository;
 
 import com.ssafy.staticsserver.domain.news.model.ForeignNewsMongo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ssafy.staticsserver.domain.news.model.DomesticNewsMongo;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
-public interface DomesticNewsMongoDBRepository extends MongoRepository<DomesticNewsMongo, String> {
-    List<ForeignNewsMongo> findByIdIn(List<String> idList);
+public interface NewsMongoDBRepository {
+    List<ForeignNewsMongo> findByIdIn(List<String> ids);
     @Query(value = "{ '_id': { $in: ?0 } }", fields = "{ 'keywords': 1, '_id': 0 }") //  조회 필드 지정 keywords만 조회
     List<KeywordsOnly> findKeywordsOnly(List<String> idList);
 }
