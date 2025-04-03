a5fb4e74ec2d987ea5c470c4f66f2f0223c913ca
g1767g <g1767g@naver.com>
2025-04-03 13:24:29 +0900

    feat: [S12P21A508-212] 키워드 랭킹에서 MongoDB 필드 keywords만 사용
    Former-commit-id: a2edaf74c94d54def27b936490918b87eb0408af

--- backend/statics-server/src/main/java/com/ssafy/staticsserver/domain/news/repository/ForeignNewsMongoDBRepository.java
+++ backend/statics-server/src/main/java/com/ssafy/staticsserver/domain/news/repository/ForeignNewsMongoDBRepository.java
@@ -3,10 +3,14 @@ package com.ssafy.staticsserver.domain.news.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.ssafy.staticsserver.domain.news.model.ForeignNewsMongo;
 
 public interface ForeignNewsMongoDBRepository extends MongoRepository<ForeignNewsMongo, String> {
 	List<ForeignNewsMongo> findByIdIn(List<String> idList);
+	// ID필드에서 $in => SQL의 IN , 리스트에 포함되는거 찾음
+	@Query(value = "{ '_id': { $in: ?0 } }", fields = "{ 'keywords': 1 }") //  조회 필드 지정 keywords만 조회
+	List<KeywordsOnly> findKeywordsOnly(List<String> idList);
 
 }
