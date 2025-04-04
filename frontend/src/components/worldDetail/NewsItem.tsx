c9dbbe63e5fdc1f8ee9dd4f6427ae75587de4070
aaange <ayung00@naver.com>
2025-04-04 15:07:24 +0900

    design: [S12P21A508-223] 상세페이지 뉴스리스트 스타일 업데이트
    Former-commit-id: 249b5d7c809d1b4484880451f455754772f65b8e

--- frontend/src/components/worldDetail/NewsItem.tsx
+++ frontend/src/components/worldDetail/NewsItem.tsx
@@ -1,3 +1,5 @@
+import { formatDate } from '../../utils/formatDateUtils';
+
 interface NewsItemProps {
   title: string;
   url: string;
@@ -15,9 +17,9 @@ const NewsItem = ({ title, url, publishedDate, imageUrl }: NewsItemProps) => {
     >
       <img src={imageUrl} alt={title} className="w-20 h-20 object-cover mr-4" />
       <div className="flex flex-col w-full justify-between">
-        <h3 className="text-lg font-semibold">{title}</h3> {/* 뉴스 제목 */}
+        <h3 className="body-medium font-semibold">{title}</h3> {/* 뉴스 제목 */}
         <span className="text-sm text-gray-500 text-end">
-          {publishedDate}
+          {formatDate(publishedDate, '')}
         </span>{' '}
         {/* 뉴스 발행 날짜 */}
       </div>
