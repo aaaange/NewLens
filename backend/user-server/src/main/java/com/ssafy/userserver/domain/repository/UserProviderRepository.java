ca4789ecbc235561f1f8776dcc7a01e1c8302898
김보민 <mokbee1@naver.com>
2025-04-05 18:48:59 +0900

    feat: [S12P21A508-219] jpa 메서드 추가

--- backend/user-server/src/main/java/com/ssafy/userserver/domain/repository/UserProviderRepository.java
+++ backend/user-server/src/main/java/com/ssafy/userserver/domain/repository/UserProviderRepository.java
@@ -2,12 +2,14 @@ package com.ssafy.userserver.domain.repository;
 
 import com.ssafy.userserver.domain.entity.UserProvider;
 import com.ssafy.userserver.domain.entity.User;
-import com.ssafy.userserver.domain.enums.ProviderType;
+import com.ssafy.userserver.domain.enums.ProviderName;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserProviderRepository extends JpaRepository<UserProvider, Long> {
-	Optional<UserProvider> findByUserAndProviderName(User user, ProviderType providerName);
+	Optional<UserProvider> findByUserAndProviderName(User user, ProviderName providerName);
 
-	Optional<UserProvider> findByUserIdAndProviderName(Long id, ProviderType providerType);
+	Optional<UserProvider> findByUserIdAndProviderName(Long id, ProviderName providerName);
+
+	Optional<UserProvider> findByUser(User user);
 }
