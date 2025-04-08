ac187d5ab26fa5e1d500e8dbcc80fb55a595bf0e
김보민 <mokbee1@naver.com>
2025-04-08 13:25:57 +0900

    feat: [S12P21A508-249] 토큰 타입을 text로 변경
    Former-commit-id: 72cf19cca130f50915af9398dd7961abca56d44f

--- backend/user-server/src/main/java/com/ssafy/userserver/domain/entity/UserProvider.java
+++ backend/user-server/src/main/java/com/ssafy/userserver/domain/entity/UserProvider.java
@@ -42,10 +42,10 @@ public class UserProvider {
 	@Column(name = "provider_id", nullable = false)
 	private String providerId;
 
-	@Column(name = "access_token")
+	@Column(name = "access_token", columnDefinition = "TEXT")
 	private String accessToken;
 
-	@Column(name = "refresh_token")
+	@Column(name = "refresh_token", columnDefinition = "TEXT")
 	private String refreshToken;
 
 	@Column(name = "created_at", nullable = false)
