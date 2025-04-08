d43a7420db3e4fc4ec514967d78c9fe7b9855d50
g1767g <g1767g@naver.com>
2025-04-08 22:42:49 +0900

    feat: [S12P21A508-260] users 테이블명 지정 및 MySQL 설정 변경
    Former-commit-id: 1606a1c72859e1bce8b117b7f1a22c5716a90aef

--- backend/statics-server/src/main/java/com/ssafy/staticsserver/domain/user/entity/User.java
+++ backend/statics-server/src/main/java/com/ssafy/staticsserver/domain/user/entity/User.java
@@ -2,8 +2,10 @@ package com.ssafy.staticsserver.domain.user.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     private Long id;
