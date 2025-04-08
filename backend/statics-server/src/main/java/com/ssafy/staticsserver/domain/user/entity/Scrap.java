2bbc2f364e726636e61022a44354c24a54fc543f
g1767g <g1767g@naver.com>
2025-04-08 21:23:19 +0900

    feat: [S12P21A508-260] 유저, 스크랩 정보 추가
    Former-commit-id: 93f537ece8c5b6458191e03a2cdf6b4a091d4dc4

---
+++ backend/statics-server/src/main/java/com/ssafy/staticsserver/domain/user/entity/Scrap.java
@@ -0,0 +1,43 @@
+package com.ssafy.staticsserver.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "scraps")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Scrap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private ScrapType type;
+
+    @Column(name = "news_id", nullable = false)
+    private String newsId;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Scrap(User user, ScrapType type, String newsId, LocalDateTime createdAt) {
+        this.user = user;
+        this.type = type;
+        this.newsId = newsId;
+        this.createdAt = createdAt;
+    }
+}
