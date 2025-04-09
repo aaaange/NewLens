12e6c422698cfccba0718286925f5bfd5baa9ca1
aaange <ayung00@naver.com>
2025-04-09 11:06:29 +0900

    feat:[S21P21A508-265] 프로필 api 코드
    Former-commit-id: 09524bb52ca1adc163b4221601dc85d8e0c42448

--- frontend/src/services/api/AuthService.ts
+++ frontend/src/services/api/AuthService.ts
@@ -4,3 +4,21 @@ export const logOutApi = async () => {
   const response = await api.post('user/auth/logout');
   return response.data;
 };
+
+// PATCH 닉네임 변경
+export const patchNicknameApi = async (nickname:string) => {
+  const response = await api.patch('user/auth/nickname', nickname);
+  return response.data;
+};
+
+// // POST 회원 탈퇴
+// export const signOutApi = async () => {
+//   const response = await api.post('user/auth/signout');
+//   return response.data;
+// };
+
+// GET 회원 정보 조회
+export const getUserInfoApi = async () => {
+  const response = await api.get('user/auth/user_info');
+  return response.data;
+};
