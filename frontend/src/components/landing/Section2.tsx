6d3a22aa169be44527fbb1d688c7ce95a25e4bbd
seungju <seungju425@gmail.com>
2025-04-09 14:21:15 +0900

    feat: [S12P21A508-248]
    - 로그인 페이지 "돌아가기" -> "비회원으로 시작"으로"변경
    - section2 변경중
    
    
    Former-commit-id: 19721290a0f8a4f66fef4375b05772961ec1181b

--- frontend/src/components/landing/Section2.tsx
+++ frontend/src/components/landing/Section2.tsx
@@ -36,82 +36,108 @@ const Section2 = forwardRef<HTMLDivElement>((_, ref) => {
   };
 
   return (
-    <div
-      ref={ref}
-      className="relative flex flex-col justify-start items-center h-screen p-[2vw]"
-    >
-      <div className="z-10 flex flex-col items-center gap-[4vh] mt-[10vh] text-center">
-        <motion.p
-          className="headline-xlarge"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: false, amount: 0.2 }}
-          variants={fadeScaleLeft}
-          custom={0}
-        >
-          뉴스도 데이터로 읽는 시대,
-        </motion.p>
+    // <div
+    //   ref={ref}
+    //   className="relative flex flex-col justify-start items-center h-screen p-[2vw]"
+    // >
+    //   <div className="z-10 flex flex-col items-center gap-[4vh] mt-[10vh] text-center">
+    //     <motion.p
+    //       className=" font-['Cafe24ClassicType-Regular']"
+    //       initial="hidden"
+    //       whileInView="visible"
+    //       viewport={{ once: false, amount: 0.2 }}
+    //       variants={fadeScaleLeft}
+    //       custom={0}
+    //     >
+    //       뉴스도 데이터로 읽는 시대,
+    //     </motion.p>
 
-        <motion.p
-          className="headline-xlarge"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: false, amount: 0.2 }}
-          variants={fadeScaleRight}
-          custom={1}
-        >
-          핫 키워드, 국가별 언급량, 감정 분석까지 한눈에!
-        </motion.p>
+    //     <motion.p
+    //       className="headline-xlarge"
+    //       initial="hidden"
+    //       whileInView="visible"
+    //       viewport={{ once: false, amount: 0.2 }}
+    //       variants={fadeScaleRight}
+    //       custom={1}
+    //     >
+    //       핫 키워드, 국가별 언급량, 감정 분석까지 한눈에!
+    //     </motion.p>
 
-        <motion.p
-          className="headline-xlarge"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: false, amount: 0.2 }}
-          variants={fadeScaleLeft}
-          custom={2}
-        >
-          각국의 뉴스 흐름을 비교하며 인사이트를 발견하세요.
-        </motion.p>
+    //     <motion.p
+    //       className="headline-xlarge"
+    //       initial="hidden"
+    //       whileInView="visible"
+    //       viewport={{ once: false, amount: 0.2 }}
+    //       variants={fadeScaleLeft}
+    //       custom={2}
+    //     >
+    //       각국의 뉴스 흐름을 비교하며 인사이트를 발견하세요.
+    //     </motion.p>
 
-        <motion.p
-          className="headline-xlarge"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: false, amount: 0.2 }}
-          variants={fadeScaleRight}
-          custom={3}
-        >
-          내 관심사를 반영한 맞춤 뉴스까지,
-        </motion.p>
+    //     <motion.p
+    //       className="headline-xlarge"
+    //       initial="hidden"
+    //       whileInView="visible"
+    //       viewport={{ once: false, amount: 0.2 }}
+    //       variants={fadeScaleRight}
+    //       custom={3}
+    //     >
+    //       내 관심사를 반영한 맞춤 뉴스까지,
+    //     </motion.p>
 
-        <motion.p
-          className="display-medium mt-[10vh]"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: false, amount: 0.2 }}
-          variants={fadeScaleUp}
-        >
-          <span className="text-amount-300">NEW</span>LEN
-          <span className="text-amount-300">S</span>에서 경험해보세요
-        </motion.p>
+    //     <motion.p
+    //       className="display-medium mt-[10vh]"
+    //       initial="hidden"
+    //       whileInView="visible"
+    //       viewport={{ once: false, amount: 0.2 }}
+    //       variants={fadeScaleUp}
+    //     >
+    //       <span className="text-amount-300">NEW</span>LEN
+    //       <span className="text-amount-300">S</span>에서 경험해보세요
+    //     </motion.p>
+    //   </div>
+
+    //   {/* 아이콘 이미지들 */}
+    //   <div className="absolute bottom-[2vh] left-0 right-0 flex justify-between px-[6vw] pointer-events-none">
+    //     <img
+    //       src="/assets/images/bar-chart.png"
+    //       className="absolute bottom-[5vh] left-[4vw] rotate-[-10deg] w-[140px] h-[140px]"
+    //     />
+    //     <img
+    //       src="/assets/images/world-grid.png"
+    //       className="absolute bottom-[5vh] right-[4vw] w-[140px] h-[140px] rotate-[15deg]"
+    //     />
+    //     <img
+    //       src="/assets/images/news-paper.png"
+    //       className="absolute bottom-[15vh] left-[18vw] w-[140px] h-[140px] rotate-[10deg]"
+    //     />
+    //   </div>
+    // </div>
+    <div>
+      {/* 숫자 */}
+      <div ref={ref} className="flex h-screen justify-between m-[20vh]">
+        <div className="flex flex-col items-center gap-8">
+          <p className="text-primary-300 display-small">COUNTRIES</p>
+          <p className="font-['Cafe24ClassicType-Regular'] text-7xl text-amount-300">
+            18
+          </p>
+        </div>
+        <div className="flex flex-col items-center gap-8">
+          <p className="text-primary-300 display-small">DAILY DATA</p>
+          <p className="font-['Cafe24ClassicType-Regular'] text-7xl text-amount-300">
+            100,000+
+          </p>
+        </div>
+        <div className="flex flex-col items-center gap-8">
+          <p className="text-primary-300 display-small">TOTAL DATA</p>
+          <p className="font-['Cafe24ClassicType-Regular'] text-7xl text-amount-300">
+            1,116,279
+          </p>
+        </div>
       </div>
 
-      {/* 아이콘 이미지들 */}
-      <div className="absolute bottom-[2vh] left-0 right-0 flex justify-between px-[6vw] pointer-events-none">
-        <img
-          src="/assets/images/bar-chart.png"
-          className="absolute bottom-[5vh] left-[4vw] rotate-[-10deg] w-[140px] h-[140px]"
-        />
-        <img
-          src="/assets/images/world-grid.png"
-          className="absolute bottom-[5vh] right-[4vw] w-[140px] h-[140px] rotate-[15deg]"
-        />
-        <img
-          src="/assets/images/news-paper.png"
-          className="absolute bottom-[15vh] left-[18vw] w-[140px] h-[140px] rotate-[10deg]"
-        />
-      </div>
+      {/* 문장 */}
+      <div></div>
     </div>
   );
 });
