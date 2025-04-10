eef0cc10d876ecb8032ad396b4dbe32df8e9b5d7
g1767g <g1767g@naver.com>
2025-04-10 23:13:27 +0900

    feat: [S12P21A508-280] 스케쥴러 시간 변경

--- backend/search-server/src/main/java/com/ssafy/searchserver/infrastructure/scheduler/KeywordRankingScheduler.java
+++ backend/search-server/src/main/java/com/ssafy/searchserver/infrastructure/scheduler/KeywordRankingScheduler.java
@@ -21,7 +21,7 @@ public class KeywordRankingScheduler {
 	/**
 	 * 매일 새벽 4시에 키워드 랭킹 통계 요청을 실행합니다.
 	 */
-	 @Scheduled(cron = "10 40 20 * * ?")
+	 @Scheduled(cron = "00 55 03 * * ?") // 03:55분으로 한국 특화 정각 피하기 위해
 	public void scheduleKeywordRanking() {
 		List<String> categories = Arrays.asList(
 			"general", "science", "sports", "business", "health",
@@ -45,8 +45,7 @@ public class KeywordRankingScheduler {
 	}
 
 
-//	@Scheduled(cron = "0 0 * * * ?") // 정각 마다
-	@Scheduled(cron = "50 43 20 * * ?")
+	@Scheduled(cron = "0 0 * * * ?") // 정각 마다
 	public void scheduleHourlyKoreaRanking() {
 		List<String> categories = Arrays.asList(
 			"general", "science", "sports", "business", "health",
