0080fdfadfb879314903fb63a8ac3a65b30f8d1f
g1767g <g1767g@naver.com>
2025-04-11 02:04:22 +0900

    feat: [S12P21A508-280] 일단 배포 서버 스케줄러 끔
    Former-commit-id: e55bd2e6e5d21c6ec3cad4311b9d4cbf02e2fbe7

--- backend/search-server/src/main/java/com/ssafy/searchserver/infrastructure/scheduler/KeywordRankingScheduler.java
+++ backend/search-server/src/main/java/com/ssafy/searchserver/infrastructure/scheduler/KeywordRankingScheduler.java
@@ -21,7 +21,7 @@ public class KeywordRankingScheduler {
 	/**
 	 * 매일 새벽 4시에 키워드 랭킹 통계 요청을 실행합니다.
 	 */
-	 @Scheduled(cron = "00 55 03 * * ?") // 03:55분으로 한국 특화 정각 피하기 위해
+//	 @Scheduled(cron = "00 55 03 * * ?") // 03:55분으로 한국 특화 정각 피하기 위해
 	public void scheduleKeywordRanking() {
 		List<String> categories = Arrays.asList(
 			"general", "science", "sports", "business", "health",
@@ -46,7 +46,7 @@ public class KeywordRankingScheduler {
 
 
 //	@Scheduled(cron = "0 0 * * * ?") // 정각 마다
-	@Scheduled(cron = "20 55 01 * * ?")
+//	@Scheduled(cron = "20 55 01 * * ?")
 	public void scheduleHourlyKoreaRanking() {
 		List<String> categories = Arrays.asList(
 			"general", "science", "sports", "business", "health",
