4cb3c633c80c26cd08f07c58d86b6f02f22a2bd6
양영조 <a10019610@gmail.com>
2025-04-11 09:31:36 +0900

    fix: qa 진행중- 6

--- frontend/src/components/common/DropdownMenu.tsx
+++ frontend/src/components/common/DropdownMenu.tsx
@@ -2,14 +2,22 @@ import { DropdownTile } from './DropdownTile';
 
 export const DropdownMenu = ({
   list,
+  name,
 }: {
   list: { text: string; onClick: () => void }[];
+  name: string;
 }) => {
   return (
-    <div className="right-[80px] w-[120px] rounded-[10px] p-1 bg-gray-0 border border-gray-200 shadow-xsmall cursor-pointer z-10">
+    <div className="right-[80px] w-[180px] rounded-[10px] p-1 bg-gray-0 border border-gray-200 shadow-xsmall cursor-pointer z-10">
+      <div className="text-gray-800 p-2 justify-center border-b border-b-gray-300 flex">
+        <span className="font-bold">{name}</span>
+        <span>님 안녕하세요!</span>
+      </div>
       {list.map((item, index) => {
         return (
-          <DropdownTile key={index} text={item.text} onClick={item.onClick} />
+          <div>
+            <DropdownTile key={index} text={item.text} onClick={item.onClick} />
+          </div>
         );
       })}
     </div>
